efab14221f125a5326e25548abf1391262a9a3c9
DDDlucky <d.ono2105@gmail.com>
2021-01-11 10:53:50 +0900

    “login方法、schema修正”

---
+++ Documents/Bookers2/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '61d5cd9427cbd26dac2df0b5f4afe24e99d9443dc3c9a4b7f68dfee925465294b74b78c427b9174c3926e872dd7f8e3291b4f818276f4f7cc8965604f3c76742'
